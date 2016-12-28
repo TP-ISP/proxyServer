bc245fe8f2dc3e45a944ebe7b852333ef4b4b071
zhangzju <zhangway19921221@163.com>
2016-12-29 01:43:54 +0800

    add request to exchangerate

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var router = require('koa-router')();
+var request = require('request');
+
+let exchangeKey = '09ff0501cea42426c20b0635a0623d2f';
+let exchangeURL = 'http://op.juhe.cn/onebox/exchange/query?key='+exchangeKey;
+let exchangeInfo = {};
 
 router.get('/', function *(next) {
-  yield this.render('index', {
-    title: 'Hello World Koa!'
-  });
+  request(exchangeURL, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(response);
+      exchangeInfo = body;
+      console.log(exchangeInfo);
+    }
+  })
+  yield this.render('index', exchangeInfo);
 });
 
 module.exports = router;
